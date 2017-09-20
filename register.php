50072ddddb4c26c9efcf5fe5ea2d2f94ccccfede
Catalinul95 <pocsan95@gmail.com>
2017-09-20 22:11:17 +0300

    Take away the HTML part and move it to its own file in the views folder

--- register.php
+++ register.php
@@ -99,40 +99,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   exit;
 }
 
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Register Page | Simple authentication system made with PHP.</title>
-  </head>
-  <body>
-      <h1>Register Page</h1>
-      <hr>
-      <?php if (isset($_SESSION['errors'])): ?>
-        <p><b>Whoops!</b> <?=$_SESSION['errors'][0];?></p>
-      <?php endif; ?>
-      <?php if (isset($_SESSION['messages'])): ?>
-        <p><b>Success!</b> <?=$_SESSION['messages'][0];?></p>
-      <?php endif; ?>
-      <form action="register.php" method="post">
-        <p>
-          Username: <input type="text" name="username" required>
-        </p>
-        <p>
-          Email: <input type="email" name="email" required>
-        </p>
-        <p>
-          Password: <input type="password" name="password" required>
-        </p>
-        <p>
-          Confirm Password: <input type="password" name="password_confirmation" required>
-        </p>
-        <button type="submit">Register</button>
-      </form>
-  </body>
-</html>
-<?php
+// include the HTML content, called view
+require __DIR__ . '/views/register.php';
+
 // remove the form validation errors from the current session
 if (isset($_SESSION['errors'])) {
   unset($_SESSION['errors']);
@@ -142,4 +111,3 @@ if (isset($_SESSION['errors'])) {
 if (isset($_SESSION['messages'])) {
   unset($_SESSION['messages']);
 }
-?>
