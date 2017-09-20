ee0bde9e281d1beaade3e386d5129ec6887a2b41
Catalinul95 <pocsan95@gmail.com>
2017-09-20 20:46:24 +0300

    Let's store form validation errors in a session

--- register.php
+++ register.php
@@ -3,13 +3,20 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// start the session mechanism
+// it gives as the power to persist data on other pages
+session_start();
+
 // if the form was submitted, then a POST request was sent to this file
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   // if one of the fields is empty ( no data was submitted )
   // TODO: make it cleaner
   if (empty($_POST['username']) || empty($_POST['email']) || empty($_POST['password']) || empty($_POST['password_confirmation'])) {
-    die('You must fill all the fields.');
+    // store form validation errors in a session
+    $_SESSION['errors'][] = 'Please fill all the fields.';
+    header('Location: register.php');
+    exit;
   }
 }
 
@@ -23,6 +30,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <body>
       <h1>Register Page</h1>
       <hr>
+      <?php if (isset($_SESSION['errors'])): ?>
+        <p><b>Whoops!</b> <?=$_SESSION['errors'][0];?></p>
+      <?php endif; ?>
       <form action="register.php" method="post">
         <p>
           Username: <input type="text" name="username" required>
@@ -40,3 +50,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       </form>
   </body>
 </html>
+<?php
+// remove the form validation errors from the current session
+if (isset($_SESSION['errors'])) {
+  unset($_SESSION['errors']);
+}
+?>
