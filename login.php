7eed4e49d3e8e5d6100a25f5c3908f33dbff9220
Catalinul95 <pocsan95@gmail.com>
2017-09-21 15:10:25 +0300

    Form validation - check if fields are empty

--- login.php
+++ login.php
@@ -4,8 +4,18 @@ require_once __DIR__  .'/init.php';
 
 // if the form was submitted, then a POST request was sent to this file
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  echo 'Submitted';
+  //FORM VALIDATION - check if one of the fields is empty
+  if (empty($_POST['email']) || empty($_POST['password'])) {
+    $_SESSION['errors'][] = 'Plese fill all the required fields.';
+    header('Location: login.php');
+    exit;
+  }
 }
 
 // include the view file used for this page
 require_once __DIR__ . '/views/login.php';
+
+// remove the form validation errors from the current session
+if (isset($_SESSION['errors'])) {
+  unset($_SESSION['errors']);
+}
