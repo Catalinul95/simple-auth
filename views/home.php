f7bf1d987c3d1a7689afefa0a0fd7f97c19bd16d
Catalinul95 <pocsan95@gmail.com>
2017-09-21 15:31:51 +0300

    The home page view file

---
+++ views/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Simple authentication system made with PHP.</title>
+  </head>
+  <body>
+      <h1>Simple authentication system</h1>
+      <hr>
+      <ul>
+        <li>
+          <a href="login.php">Login page</a>
+        </li>
+        <li>
+          <a href="register.php">Register Page</a>
+        </li>
+      </ul>
+      <p>
+        <small>This is a simple authentication system made with PHP, having MySQL as the database.It uses the procedural style.</small>
+      </p>
+  </body>
+</html>
