e8b5fca0351f5727092a7d02d9ad00e99b5eeb37
Catalinul95 <pocsan95@gmail.com>
2017-09-21 15:41:37 +0300

    Replace wrong text

--- views/login.php
+++ views/login.php
@@ -5,17 +5,17 @@
     <title>Login Page | Simple authentication system made with PHP.</title>
   </head>
   <body>
-      <h1>Register Page</h1>
+      <h1>Login Page</h1>
       <hr>
       <?php if (isset($_SESSION['errors'])): ?>
         <p><b>Whoops!</b> <?=$_SESSION['errors'][0];?></p>
       <?php endif; ?>
       <form action="login.php" method="post">
         <p>
-          Email: <input type="email" name="email" required>
+          Email: <input type="email" name="email" >
         </p>
         <p>
-          Password: <input type="password" name="password" required>
+          Password: <input type="password" name="password" >
         </p>
         <button type="submit">Login</button>
       </form>
