0df6f7fc2a7bc4fa4ff2b70dee715ead0bd61cc0
Catalinul95 <pocsan95@gmail.com>
2017-09-21 15:01:27 +0300

    Move initialisation code in its own file

--- register.php
+++ register.php
@@ -1,20 +1,5 @@
 <?php
-// make sure all errors are displayed
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-// start the session mechanism
-// it gives us the power to persist data on other pages
-session_start();
-
-// TODO: move it to its own file
-// connect to database
-$db = mysqli_connect("127.0.0.1", "root", "", "simple_auth");
-
-// check connection status
-if (mysqli_connect_errno()) {
-  echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
-}
+require_once __DIR__ . '/init.php';
 
 // if the form was submitted, then a POST request was sent to this file
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
