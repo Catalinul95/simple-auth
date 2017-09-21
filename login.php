ecf4e39f321a6989f90185f47dadcaa6effa8d45
Catalinul95 <pocsan95@gmail.com>
2017-09-21 15:07:58 +0300

    Check if a POST request was sent to the login page

--- login.php
+++ login.php
@@ -2,5 +2,10 @@
 // include the initialisation file
 require_once __DIR__  .'/init.php';
 
+// if the form was submitted, then a POST request was sent to this file
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  echo 'Submitted';
+}
+
 // include the view file used for this page
 require_once __DIR__ . '/views/login.php';
