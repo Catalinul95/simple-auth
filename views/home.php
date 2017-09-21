5afe1a21c69c6c5a91b080bc169afd4d984279bc
Catalinul95 <pocsan95@gmail.com>
2017-09-21 15:35:31 +0300

    Show a message if the user is logged

--- views/home.php
+++ views/home.php
@@ -7,6 +7,10 @@
   <body>
       <h1>Simple authentication system</h1>
       <hr>
+      <?php if (isset($_SESSION['user'])): ?>
+        <h3>Welcome back, <?=$_SESSION['user']['username'];?>!</h3>
+        <a href="logout.php">Logout</a>
+      <?php else: ?>
       <ul>
         <li>
           <a href="login.php">Login page</a>
@@ -15,6 +19,8 @@
           <a href="register.php">Register Page</a>
         </li>
       </ul>
+      <?php endif; ?>
+
       <p>
         <small>This is a simple authentication system made with PHP, having MySQL as the database.It uses the procedural style.</small>
       </p>
